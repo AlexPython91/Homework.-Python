ad18128f2d39d3cff2e1256423aa09ef7b216e20
alexhvorostov <hvorostov.alex1@gmail.com>
2022-01-31 15:47:26 +0400

    Added task 13. Find occurrences

---
+++ 13. Find occurrences.py
@@ -0,0 +1,7 @@
+# 13. Пользователь задаёт две строки. 
+# Определить количество количество вхождений одной строки в другой.
+
+first_str = input('Your first line: ')
+second_str = input('Your second line: ')
+
+print(second_str.find(first_str))
