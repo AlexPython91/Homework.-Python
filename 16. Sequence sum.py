bf11770332b19fc81bbee0922ef089c66ba093c9
alexhvorostov <hvorostov.alex1@gmail.com>
2022-01-31 15:53:40 +0400

    Added task 16. Sequence sum

---
+++ 16. Sequence sum.py
@@ -0,0 +1,8 @@
+# 16. Задать список из n чисел последовательности  и вывести на экран их сумму
+
+from random import randint
+
+my_list = [randint(1, 10) for i in range(10)]
+print(my_list)
+# print(sum(my_list))
+
