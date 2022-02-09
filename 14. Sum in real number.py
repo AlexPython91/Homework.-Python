ddf77d739d3551e42c03b036dad53510b6c51894
alexhvorostov <hvorostov.alex1@gmail.com>
2022-02-09 16:33:12 +0400

    Added task 14. Sum of real number

--- 14. Sum in real number.py
+++ 14. Sum in real number.py
@@ -1,9 +1,8 @@
 # 14. Подсчитать сумму цифр в вещественном числе 
 
-num = 2.455679
+num = 2.455678
 
 def sum_numbers(number):
-    # x = str(number)
     res = 0
     for i in str(number):
         if i != '.':
